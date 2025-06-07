c589b3a5121a269bfbb0614195579f07d3418fdf
Avinashr100 <avinash.official1@gmail.com>
2025-06-07 15:48:20 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+from data.google_sheets_connector import load_data
 st.set_page_config(page_title="Investment in Securities Oversight Board", layout="wide")
 
 st.markdown("""
